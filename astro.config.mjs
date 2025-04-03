f81f23a9dbfdabe5e8a9a0cd26b9a2e23f050e4a
Rebecca Alitzel <nekashii@outlook.com>
2025-04-03 02:20:15 -0600

    feat: add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://horas-juego.nekashii.dev',
+  redirects: {
+    "/lana": "/profiles/76561199441037031/stats/346110"
+  },
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
